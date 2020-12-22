ffd3fa1a4c59a2a4a63243f7430b10dabe649b81
artur <58606521+hackers-pr@users.noreply.github.com>
2020-12-22 19:59:39 +0300

    fix

--- cookiecatcher.py
+++ cookiecatcher.py
@@ -1,12 +1,7 @@
-import requests, re, json, random, os, threading
+import requests, re, json, random, os
 from time import sleep
 
 class cookiecatcher():
-
-    def __init__(self):
-        self.next_page=None,
-        self.STARTURLS=None,
-        self.USERAGENT=None
 
     def configload(self):
 
@@ -19,17 +14,16 @@ class cookiecatcher():
             self.SLOWDOWN=config['SLOWDOWN']
             self.SLOWDOWN_RANGE=config['SLOWDOWN_RANGE']
 
-    def parsenext(self):
-        print(self.next_page)
-        dont_write=False; domain=re.sub(r'[?]', '/', self.next_page).split('/')[2]; current_page=self.next_page
+    def request(self):
+        self.domain=re.sub(r'[?]', '/', self.next_page).split('/')[2]; self.current_page=self.next_page
 
         with open('Cookies/cookies.json', 'r') as raw_cookies:
             cookies=raw_cookies.read()
             current_cookies=json.loads(cookies)
 
-            try:
-                cookie=current_cookies[domain]
-            except:
+            if current_cookies.get(self.domain)!=None:
+                cookie=current_cookies[self.domain]
+            else:
                 cookie=None
 
         try:
@@ -37,18 +31,19 @@ class cookiecatcher():
                 self.page=requests.get(self.next_page, headers={'user-agent': self.USERAGENT})
             else:
                 self.page=requests.get(self.next_page, headers={'user-agent': self.USERAGENT, 'cookie': cookie})
-        
+
         except:
             print('An error was occured, the next url will be selected from the STARTURLS')
             self.next_page=random.choice(self.STARTURLS)
 
-        res=[re.sub('<a href="(http[s]*://.+)"', r'\1', raw_url) for raw_url in re.findall('<a href="http[s]*://.*?"', self.page.text)]; err=True
+    def parse(self):
+        print(self.next_page)
+        dont_write=False
 
-        while err:
-            try:
-                res.remove(current_page)
-            except:
-                err=False
+        res=[re.sub('<a href="(http[s]*://.+)"', r'\1', raw_url) for raw_url in re.findall('<a href="http[s]*://.*?"', self.page.text)]
+
+        while res.count(self.current_page)!=0:
+            res.remove(self.current_page)
 
         if len(res)>0:
             self.next_page=random.choice([re.sub('<a href="(.+)"', r'\1', x) for x in res])
@@ -56,9 +51,9 @@ class cookiecatcher():
             print('Dead end, the next url will be selected from the STARTURLS')
             self.next_page=random.choice(self.STARTURLS)
 
-        try:
+        if self.page.headers.get('set-cookie')!=None:
             cookie=self.page.headers['set-cookie']
-        except:
+        else:
             dont_write=True
 
         if not dont_write:
@@ -67,27 +62,18 @@ class cookiecatcher():
                 old_cookies=json.loads(cookies); current_cookies=old_cookies
 
             with open('Cookies/cookies.json', 'w') as raw_cookies:
-                current_cookies.update({domain: cookie})
+                current_cookies.update({self.domain: cookie})
                 raw_cookies.write(json.dumps(current_cookies, indent=4))
 
         with open('Cookies/history.log', 'a') as history:
-            history.write(current_page+'\n')
+            history.write(self.current_page+'\n')
 
         if self.SLOWDOWN:
             sleep(random.randint(self.SLOWDOWN_RANGE[0], self.SLOWDOWN_RANGE[1]))
 
-def threading_input():
-    global user_input
-    user_input=None
-
-    while user_input==None:
-        user_input=input('If you want to exit, press ENTER\n')
-        print('Exiting...')
-
-threading.Thread(target=threading_input).start()
-
 if __name__=="__main__":
-    cc, user_input=cookiecatcher(), None
+    cc=cookiecatcher()
     cc.configload()
-    while user_input==None:
-        cc.parsenext()
+    while True:
+        cc.request()
+        cc.parse()
